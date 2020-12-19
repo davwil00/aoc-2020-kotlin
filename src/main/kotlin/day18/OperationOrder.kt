3424c3aa950cf02e9dfab2115d62cd7b4b7eb59b
David Williams <davwil00@gmail.com>
2020-12-19 20:06:03 +0100

    Day 18 solution

--- src/main/kotlin/day18/OperationOrder.kt
+++ src/main/kotlin/day18/OperationOrder.kt
@@ -1,33 +1,102 @@
 package day18
 
-class OperationOrder {
+import java.io.File
+import java.lang.IllegalStateException
 
-    fun calculate(line: String) {
-        val input = line.replace(" ", "").toCharArray()
-        val equation = parseInput(input)
+class OperationOrder() {
 
+    fun sumEquationResults(input: List<String>): Long {
+        return input.map { calculate(it) }.sum()
     }
 
-    fun parseInput(charArray: CharArray): Equation {
+    fun calculate(line: String): Long {
+        val input = line.replace(" ", "").toCharArray()
+        val equation = parseInput(input)
+        return equation.solve()
+    }
+
+    private fun parseInput(charArray: CharArray): Equation {
         val char0 = charArray[0]
         if (charArray.size == 1) {
-            return Equation(char0.toInt())
+            return Equation(char0.intVal())
         }
-//        var equations = mutableListOf<Equation>()
-        if (!isBracket(char0)) {
-            val currentEquation = Equation(char0.toInt())
+        return if (isBracket(char0)) {
+            val (bracketedEquationInput, idx) = findMatchingBracket(charArray.sliceArray(1 until charArray.size))
+            val bracketedEquation = parseInput(bracketedEquationInput)
+            val restOfEqnChars = charArray.sliceArray(idx + 3 until charArray.size)
+            return if (restOfEqnChars.isNotEmpty()) {
+                val restOfEquation = parseInput(restOfEqnChars)
+                Equation(bracketedEquation.solve(), restOfEquation, Operator.fromChar(charArray[idx + 2]))
+            } else {
+                Equation(bracketedEquation.solve())
+            }
+        } else {
             if (isOperator(charArray[1])) {
-                currentEquation.operator = Operator.fromChar(charArray[1])
-                currentEquation.right = parseInput(charArray.sliceArray(2..charArray.size))
+                val left = char0.intVal()
+                val operator = Operator.fromChar(charArray[1])
+                val right = parseInput(charArray.sliceArray(2 until charArray.size))
+                Equation(left, right, operator)
+            } else {
+                throw IllegalStateException("Unable to parse input $charArray")
             }
         }
     }
 
-    private fun isBracket(char: Char) = char == '(' || char == ')'
-    private fun isOperator(char: Char) = char == '+' && char == '*'
+    private fun findMatchingBracket(charArray: CharArray): Pair<CharArray, Int> {
+        var brackets = 1
+        charArray.forEachIndexed { idx, char ->
+            when (char) {
+                '(' -> brackets++
+                ')' -> brackets--
+            }
 
-    enum class Operator(val sign: Char) {
-        ADD('+'), MULTIPLY('*');
+            if (brackets == 0) {
+                return Pair(charArray.sliceArray(0 until idx), idx)
+            }
+        }
+
+        throw IllegalStateException("Unable to find matching bracket")
+    }
+
+    private fun isBracket(char: Char) = char == '(' || char == ')'
+    private fun isOperator(char: Char) = char == '+' || char == '*'
+
+    enum class Operator(val sign: Char, val fn: (Long, Long) -> Long) {
+        ADD('+', Long::plus), MULTIPLY('*', Long::times);
+
+        fun applyLtoR(left: Long, right: Equation): Long {
+            println("applying $left $sign $right")
+            return when {
+                right.right == null -> {
+                    println("right is false processing $left $sign ${right.left}")
+                    fn(left, right.left)
+                }
+                right.operator != null -> {
+                    println("processing $left $sign ${right.left} ${right.operator.sign} ${right.right}")
+                    right.operator.applyLtoR(fn(left, right.left), right.right)
+                }
+                else -> throw IllegalStateException("Unable to apply operator")
+            }
+        }
+
+        fun applyPlusPrecedence(left: Long, right: Equation): Long {
+            println("applying $left $sign $right")
+            return when {
+                right.right == null -> {
+                    println("right is false processing $left $sign ${right.left}")
+                    fn(left, right.left)
+                }
+                right.operator != null -> {
+                    println("processing $left $sign ${right.left} ${right.operator.sign} ${right.right}")
+                    if (this == ADD) {
+                        right.operator.applyPlusPrecedence(fn(left, right.left), right.right)
+                    } else {
+                        fn(left, right.solve())
+                    }
+                }
+                else -> throw IllegalStateException("Unable to apply operator")
+            }
+        }
 
         companion object {
             fun fromChar(char: Char): Operator {
@@ -37,5 +106,24 @@ class OperationOrder {
         }
     }
 
-    data class Equation(val left: Int, var right: Equation? = null, var operator: Operator? = null)
+    data class Equation(val left: Long,
+                        val right: Equation? = null,
+                        val operator: Operator? = null) {
+
+        fun solve(): Long {
+            return if (operator == null || right == null) {
+                left
+            } else {
+                operator.applyPlusPrecedence(left, right)
+                //operator.applyLtoR(left, right)
+            }
+        }
+    }
+
+    private fun Char.intVal() = Character.digit(this, 10).toLong()
+}
+
+fun main() {
+    val input = File("src/main/resources/day18/input.txt").readLines()
+    println(OperationOrder().sumEquationResults(input)) // 18213007238947
 }
--- src/test/kotlin/day18/OperationOrderTest.kt
+++ src/test/kotlin/day18/OperationOrderTest.kt
@@ -2,11 +2,45 @@ package day18
 
 import org.junit.jupiter.api.Test
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 internal class OperationOrderTest {
 
     @Test
     fun `should calculate simple correctly`() {
-        assertThat(OperationOrder().calculate("1 + 2 * 3 + 4 * 5 + 6").isEqualTo(71)
+        assertThat(OperationOrder().calculate("1 + 2 * 3 + 4 * 5 + 6")).isEqualTo(71L)
+    }
+
+    @Test
+    fun `should calculate with brackets`() {
+        assertThat(OperationOrder().calculate("1 + (2 * 3) + (4 * (5 + 6))")).isEqualTo(51L)
+    }
+
+    @ParameterizedTest
+    @CsvSource("2 * 3 + (4 * 5),26",
+        "5 + (8 * 3 + 9 + 3 * 4 * 3),437",
+        "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4)),12240",
+        "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2,13632"
+    )
+    fun `additional checks`(input: String, expected: Long) {
+        assertThat(OperationOrder().calculate(input)).isEqualTo(expected)
+    }
+
+    @Test
+    fun `should calculate simple correctly with plus precedence`() {
+        assertThat(OperationOrder().calculate("1 + 2 * 3 + 4 * 5 + 6")).isEqualTo(231L)
+    }
+
+
+    @ParameterizedTest
+    @CsvSource("1 + (2 * 3) + (4 * (5 + 6)),51",
+        "2 * 3 + (4 * 5),46",
+        "5 + (8 * 3 + 9 + 3 * 4 * 3),1445",
+        "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4)),669060",
+        "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2,23340"
+    )
+    fun `additional checks with plus precedence`(input: String, expected: Long) {
+        assertThat(OperationOrder().calculate(input)).isEqualTo(expected)
     }
 }
