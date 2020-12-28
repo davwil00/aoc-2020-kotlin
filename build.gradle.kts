dc72eaab32daa6a492600e2ba5a4b391a85f9d1e
David Williams <davwil00@gmail.com>
2020-12-28 14:59:03 +0100

    Logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 
 dependencies {
     implementation("org.slf4j:slf4j-api:1.7.9")
+    implementation("ch.qos.logback:logback-classic:1.2.3")
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testImplementation("org.assertj:assertj-core:3.18.1")
