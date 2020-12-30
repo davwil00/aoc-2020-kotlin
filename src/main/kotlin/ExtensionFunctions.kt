2b4e41f365fe4ec4f979fcd7054bde7adacca3c2
David Williams <davwil00@gmail.com>
2020-12-30 14:49:55 +0000

    More extension functions

--- src/main/kotlin/ExtensionFunctions.kt
+++ src/main/kotlin/ExtensionFunctions.kt
@@ -1,2 +1,17 @@
 operator fun Pair<Int, Int>.plus(other: Pair<Int, Int>): Pair<Int, Int> = Pair(this.first + other.first, this.second + other.second)
 operator fun Pair<Int, Int>.times(multiplier: Int): Pair<Int, Int> = Pair(this.first * multiplier, this.second * multiplier)
+
+fun List<String>.rotate(): List<String> {
+    val newData = mutableListOf<String>()
+    this.indices.forEach { col ->
+        val line = mutableListOf<Char>()
+        (this.size - 1 downTo 0).forEach { row ->
+            line.add(this[row][col])
+        }
+        newData.add(line.joinToString(""))
+    }
+
+    return newData
+}
+
+fun List<String>.flipVertical() = this.map { line -> line.reversed() }
