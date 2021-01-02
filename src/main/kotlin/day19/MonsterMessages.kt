91c2f793f579b950aad97a0b603c22f76bb2705b
David Williams <davwil00@gmail.com>
2021-01-02 16:15:41 +0000

    Finish day 20 (at last)

--- src/main/kotlin/day19/MonsterMessages.kt
+++ src/main/kotlin/day19/MonsterMessages.kt
@@ -1,51 +1,95 @@
 package day19
 
+import org.slf4j.LoggerFactory
 import java.io.File
 import java.lang.IllegalStateException
 
 class MonsterMessages {
 
+    private val logger = LoggerFactory.getLogger(MonsterMessages::class.java)
+
     fun validateMessages(input: String): Int {
         val (rules, messages) = input.split("\n\n")
-        val ruleMap = processRules(rules.split("\n"))
-        val pattern = resolveRules(ruleMap[0]!!, ruleMap, "")
-        val regex = Regex(pattern)
-        return messages.split("\n").count { regex.matches(it) }
+        val ruleMap = processRules(rules.lines())
+        val patterns = resolveRules(ruleMap).map { it }.toSet()
+        return messages.lines()
+            .count {
+                patterns.contains(it)
+            }
+    }
+
+    fun validateMessagesWithLoops(input: String): Int {
+        val (rules, messages) = input.split("\n\n")
+        val ruleMap = processRules(rules.lines()).toMutableMap()
+        val resolvedRules = mutableMapOf<Int, List<String>>()
+        resolveRules(ruleMap, resolvedRules = resolvedRules).toSet()
+        val chunkSize = minOf(resolvedRules.getValue(42).first().length)
+        return messages.lines().count { message ->
+            val chunks = message.chunked(chunkSize)
+            val numberOfRule42Matches = chunks.takeWhile { it in resolvedRules.getValue(42) }.size
+            val numberOfRule31Matches =  chunks.dropWhile { it in resolvedRules.getValue(42) }
+                .takeWhile { it in resolvedRules.getValue(31) }.size
+            (numberOfRule42Matches + numberOfRule31Matches == message.length / chunkSize &&
+                    numberOfRule42Matches > numberOfRule31Matches &&
+                    numberOfRule31Matches > 0).also {
+                if (it) {
+                    logger.debug(message)
+                }
+            }
+        }
     }
 
     private fun processRules(rules: List<String>): Map<Int, Rule> {
         val ruleMap = mutableMapOf<Int, Rule>()
         rules.forEach {
-            if (LinkRule.matches(it)) {
-                val rule = LinkRule.from(it)
-                ruleMap[rule.ruleNum] = rule
-            } else if (AbsoluteRule.matches(it)) {
-                val rule = AbsoluteRule.from(it)
-                ruleMap[rule.ruleNum] = rule
-            } else {
-                throw IllegalStateException("Unmatched input $it")
+            when {
+                LinkRule.matches(it) -> {
+                    val rule = LinkRule.from(it)
+                    ruleMap[rule.ruleNum] = rule
+                }
+                AbsoluteRule.matches(it) -> {
+                    val rule = AbsoluteRule.from(it)
+                    ruleMap[rule.ruleNum] = rule
+                }
+                else -> throw IllegalStateException("Unmatched input $it")
             }
         }
 
         return ruleMap
     }
 
-    private fun resolveRules(currentRule: Rule, rules: Map<Int, Rule>, pattern: String): String {
+    private fun resolveRules(rules: Map<Int, Rule>, currentRule: Rule = rules[0]!!, resolvedRules: MutableMap<Int, List<String>> = mutableMapOf()): List<String> {
+        if (currentRule.ruleNum in resolvedRules) {
+            return resolvedRules.getValue(currentRule.ruleNum)
+        }
         return when (currentRule) {
-            is AbsoluteRule -> pattern + currentRule.value
+            is AbsoluteRule -> listOf(currentRule.value)
             is LinkRule -> {
-                val resolvedLinkRule = currentRule.linkedRules.map { linkedRule ->
-                    resolveRules(rules[linkedRule]!!, rules, pattern)
-                }.joinToString("")
+                val resolvedLinkRules = currentRule.linkedRules.map { linkedRule ->
+                    resolveRules(rules, rules[linkedRule]!!, resolvedRules)
+                }.reduce(mergeLists())
+
                 if (currentRule.orLinkedRules != null) {
-                    val resolvedOrLinkRule = currentRule.orLinkedRules.map { orLinkedRule ->
-                        resolveRules(rules[orLinkedRule]!!, rules, pattern)
-                    }.joinToString("")
-                    return "($resolvedLinkRule|$resolvedOrLinkRule)"
+                    val resolvedOrLinkRules = currentRule.orLinkedRules.map { orLinkedRule ->
+                        resolveRules(rules, rules[orLinkedRule]!!, resolvedRules)
+                    }.reduce(mergeLists())
+
+                    val resolvedRulesAndOrRules = listOf(resolvedLinkRules, resolvedOrLinkRules).flatten()
+                    resolvedRules[currentRule.ruleNum] = resolvedRulesAndOrRules
+                    return resolvedRulesAndOrRules
                 }
-                return resolvedLinkRule
+                resolvedRules[currentRule.ruleNum] = resolvedLinkRules
+                return resolvedLinkRules
             }
             else -> throw IllegalStateException("Unknown rule")
+        }
+    }
+
+    private fun mergeLists() = { acc: List<String>, curr: List<String> ->
+        acc.flatMap { outer ->
+            curr.map { inner ->
+                outer + inner
+            }
         }
     }
 
@@ -85,5 +129,6 @@ class MonsterMessages {
 
 fun main() {
     val input = File("src/main/resources/day19/input.txt").readText()
-    println(MonsterMessages().validateMessages(input))
+    println(MonsterMessages().validateMessages(input)) // 151
+    println(MonsterMessages().validateMessagesWithLoops(input)) // 386
 }
